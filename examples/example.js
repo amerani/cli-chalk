0e54d0cf611fd1ca9adc3de6d65ceeb346237ff4
Alek Merani <alekmerani@gmail.com>
2018-09-10 21:09:34 -0500

    Add support fluent logger with data

--- examples/example.js
+++ examples/example.js
@@ -11,19 +11,17 @@ log.warn("im warning you...");
 
 log.info("for your information");
 
+console.log("\n");
+
 const { logger } = log;
 
-logger.success("i have data")
-.data("name", "alek")
-.data("json", JSON.stringify({ "y": 1, 2: 3, 4: " "}, null, 4))
-.data("json2", { "y": 1, 2: 3, 4: " "})
-.data('nl', null)
-.data(null, null)
-.data("undef", undefined)
-.data(undefined, undefined)
-.data("f", 0)
-.data(true, true)
-.data(false, false)
-.data(NaN, NaN)
-.data(112342342341243, 5555)
-.write();
+const data = {
+    userId: 313
+}
+
+logger
+    .error("i have data")
+    .data("errorCode", -1000)
+    .data("timestamp", new Date())
+    .data("body", data)
+    .write();
--- index.js
+++ index.js
@@ -18,7 +18,22 @@ const _logger = {
             _logger.severity = 'success'
             _logger.message = message;
             return _logger;
-        }
+        },
+        error: (message) => {
+            _logger.severity = 'error'
+            _logger.message = message;
+            return _logger;
+        },
+        warn: (message) => {
+            _logger.severity = 'warn'
+            _logger.message = message;
+            return _logger;
+        },
+        info: (message) => {
+            _logger.severity = 'info'
+            _logger.message = message;
+            return _logger;
+        },                 
     },
     data: (key, value) => {
         _logger.dataMap[key] = value;
@@ -40,21 +55,60 @@ const _logger = {
         switch(_logger.severity) {
             case 'none':
                 console.log(`${_logger.config.nameColor(_logger.config.name)} ${_logger.config.defaultColor(_logger.message)}`);
+                Object.keys(_logger.dataMap).forEach(key => {
+                    const leftPad = _logger.config.name.length;
+                    const value = _logger.dataMap[key];
+                    const output = buildKeyValueOutput(leftPad, key, value, _logger.config.defaultColor);
+                    console.log(output)
+                })
                 break;
             case 'success':
                 console.log(`${_logger.config.nameColor(_logger.config.name)} ${_logger.config.successColor(_logger.message)}`);
                 Object.keys(_logger.dataMap).forEach(key => {
-                    let value = _logger.dataMap[key];
-                    const keyStr = key != null && typeof key === 'object' ? JSON.stringify(key) : (key != null ? key.toString() : "")
-                    const valueStr = value != null && (typeof value === 'object') ? JSON.stringify(value) : (value != null ? value.toString() : "")
-                    const padding = _logger.config.name.split("").map(_ => " ").join("");
-                    const keyPadding = keyStr.split("").map(_ => " ").join("");
-                    value = valueStr.split("").map(c => c === '\n' ? `\n${padding}${keyPadding}    `: c).join("");
-                    console.log(`${padding} ${_logger.config.defaultColor(`${key} = ${value}`)}`)
+                    const leftPad = _logger.config.name.length;
+                    const value = _logger.dataMap[key];
+                    const output = buildKeyValueOutput(leftPad, key, value, _logger.config.successColor);
+                    console.log(output)
                 })
                 break;
+            case 'error':
+                console.log(`${_logger.config.nameColor(_logger.config.name)} ${_logger.config.errorColor(_logger.message)}`);
+                Object.keys(_logger.dataMap).forEach(key => {
+                    const leftPad = _logger.config.name.length;
+                    const value = _logger.dataMap[key];
+                    const output = buildKeyValueOutput(leftPad, key, value, _logger.config.errorColor);
+                    console.log(output)
+                })
+                break;
+            case 'warn':
+                console.log(`${_logger.config.nameColor(_logger.config.name)} ${_logger.config.warnColor(_logger.message)}`);
+                Object.keys(_logger.dataMap).forEach(key => {
+                    const leftPad = _logger.config.name.length;
+                    const value = _logger.dataMap[key];
+                    const output = buildKeyValueOutput(leftPad, key, value, _logger.config.warnColor);
+                    console.log(output)
+                })
+                break;
+            case 'info':
+                console.log(`${_logger.config.nameColor(_logger.config.name)} ${_logger.config.infoColor(_logger.message)}`);
+                Object.keys(_logger.dataMap).forEach(key => {
+                    const leftPad = _logger.config.name.length;
+                    const value = _logger.dataMap[key];
+                    const output = buildKeyValueOutput(leftPad, key, value, _logger.config.infoColor);
+                    console.log(output)
+                })
+                break;                                                
         }
     }
+}
+
+function buildKeyValueOutput(leftPad, key, value, color) {
+    const padding = Array.from({ length: leftPad + 1}).join(" ");
+    const keyStr = key != null && typeof key === 'object' ? JSON.stringify(key) : (key != null ? key.toString() : "")
+    const valueStr = value != null && (typeof value === 'object') ? JSON.stringify(value) : (value != null ? value.toString() : "")
+    const keyPadding = keyStr.split("").map(_ => " ").join("");
+    value = valueStr.split("").map(c => c === '\n' ? `\n${padding}${keyPadding}    `: c).join("");
+    return `${padding} ${color(`${key} = ${value}`)}`;
 }
 
 module.exports = _logger;
