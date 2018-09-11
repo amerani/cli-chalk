d71ef4c1deeb8df14b082901915aef9852c674cc
Alek Merani <alekmerani@gmail.com>
2018-09-10 20:48:41 -0500

    Seed logger interface with data

--- examples/example.js
+++ examples/example.js
@@ -10,3 +10,20 @@ log.error("this is not right :(");
 log.warn("im warning you...");
 
 log.info("for your information");
+
+const { logger } = log;
+
+logger.success("i have data")
+.data("name", "alek")
+.data("json", JSON.stringify({ "y": 1, 2: 3, 4: " "}, null, 4))
+.data("json2", { "y": 1, 2: 3, 4: " "})
+.data('nl', null)
+.data(null, null)
+.data("undef", undefined)
+.data(undefined, undefined)
+.data("f", 0)
+.data(true, true)
+.data(false, false)
+.data(NaN, NaN)
+.data(112342342341243, 5555)
+.write();
--- index.js
+++ index.js
@@ -1,26 +1,60 @@
 const chalk = require("chalk");
 
-const logger = {
+const _logger = {
+    dataMap: {},
+    severity: 'none',
+    message: '',
     config: {
         name: "cli",
         nameColor: chalk.bgBlue,
+        defaultColor: chalk.white,
         successColor: chalk.green,
         errorColor: chalk.red,
         warnColor: chalk.keyword('orange'),
         infoColor: chalk.blue
     },
+    logger: {
+        success: (message) => {
+            _logger.severity = 'success'
+            _logger.message = message;
+            return _logger;
+        }
+    },
+    data: (key, value) => {
+        _logger.dataMap[key] = value;
+        return _logger;
+    },
     success(text) {
-        console.log(`${this.config.nameColor(this.config.name)} ${this.config.successColor(text)}`);
+        console.log(`${_logger.config.nameColor(_logger.config.name)} ${_logger.config.successColor(text)}`);
     },
     error(text) {
-        console.log(`${this.config.nameColor(this.config.name)} ${this.config.errorColor(text)}`);
+        console.log(`${_logger.config.nameColor(_logger.config.name)} ${_logger.config.errorColor(text)}`);
     },
     warn(text) {
-        console.log(`${this.config.nameColor(this.config.name)} ${this.config.warnColor(text)}`);
+        console.log(`${_logger.config.nameColor(_logger.config.name)} ${_logger.config.warnColor(text)}`);
     },
     info(text) {
-        console.log(`${this.config.nameColor(this.config.name)} ${this.config.infoColor(text)}`);
+        console.log(`${_logger.config.nameColor(_logger.config.name)} ${_logger.config.infoColor(text)}`);
+    },
+    write() {
+        switch(_logger.severity) {
+            case 'none':
+                console.log(`${_logger.config.nameColor(_logger.config.name)} ${_logger.config.defaultColor(_logger.message)}`);
+                break;
+            case 'success':
+                console.log(`${_logger.config.nameColor(_logger.config.name)} ${_logger.config.successColor(_logger.message)}`);
+                Object.keys(_logger.dataMap).forEach(key => {
+                    let value = _logger.dataMap[key];
+                    const keyStr = key != null && typeof key === 'object' ? JSON.stringify(key) : (key != null ? key.toString() : "")
+                    const valueStr = value != null && (typeof value === 'object') ? JSON.stringify(value) : (value != null ? value.toString() : "")
+                    const padding = _logger.config.name.split("").map(_ => " ").join("");
+                    const keyPadding = keyStr.split("").map(_ => " ").join("");
+                    value = valueStr.split("").map(c => c === '\n' ? `\n${padding}${keyPadding}    `: c).join("");
+                    console.log(`${padding} ${_logger.config.defaultColor(`${key} = ${value}`)}`)
+                })
+                break;
+        }
     }
 }
 
-module.exports = logger;
+module.exports = _logger;
